c517e121f14d692bb50427927439cbe6c6ded7d7
Ethan Hann <ethanhann@gmail.com>
2015-01-17 18:28:02 -0500

    Create dashboard home

--- app/app.js
+++ app/app.js
@@ -1,9 +1,36 @@
-(function (angular) {
-    'use strict';
-    var apiUrl = 'http://localhost:9001/api';
-    function mainController ($resource) {
-        $resource.get()
-    }
-    angular.module('inEngine', ['ngResource'])
-    .controller('MainCtrl', mainController);
-}(angular));
+'use strict';
+
+function inEngineConfig($stateProvider, $urlRouterProvider, eehNavigationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('inEngine', {
+            abstract: true,
+            templateUrl: 'app.html'
+        })
+        .state('inEngine.home', {
+            url: '/',
+            controller: 'HomeController',
+            templateUrl: 'home/home.html'
+        });
+
+    eehNavigationProvider.searchIsVisible(false);
+    eehNavigationProvider.sidebarTextCollapseIsCollapsed(true);
+    eehNavigationProvider.navbarBrand.text = 'InEngine.NET Dashboard';
+    eehNavigationProvider.navbarBrand.state = 'inEngine.home';
+
+    eehNavigationProvider
+        .sidebarMenuItem('home', {
+            text: 'Home',
+            iconClass: 'fa-home',
+            state: 'inEngine.home'
+        });
+}
+
+angular.module('inEngine', [
+    'angularMoment',
+    'eehNavigation',
+    'ngResource',
+    'ui.bootstrap',
+    'ui.router'
+]).config(inEngineConfig);
---
+++ app/home/home.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function homeController($scope, $resource) {
+    var apiUrl = 'http://localhost:9001/api';
+    $resource(apiUrl + '/CronTrigger').query()
+    .$promise
+    .then(function (data) {
+        $scope.cronTriggers = data;
+    });
+
+    $resource(apiUrl + '/SimpleTrigger').query()
+    .$promise
+    .then(function (data) {
+        $scope.cronTriggers = data;
+    });
+
+    $resource(apiUrl + '/Job').query()
+    .$promise
+    .then(function (data) {
+        $scope.cronTriggers = data;
+    });
+}
+
+angular.module('inEngine').controller('HomeController', homeController);
