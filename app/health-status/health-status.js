564273ab173b39b81303eaded49d7b34dda31c64
Ethan Hann <eh2413@columbia.edu>
2015-01-21 12:30:34 -0500

    Upgrade API module and rework health directive

--- app/health-status/health-status.js
+++ app/health-status/health-status.js
@@ -7,23 +7,27 @@
             scope: { healthStatus: '=healthStatus' },
             templateUrl: 'health-status/health-status.html',
             link: function (scope) {
-                if (angular.isUndefined(scope.healthStatus)) {
-                    return;
-                }
-                scope.servers = [
-                    {
-                        name: 'Mail Server',
-                        isAvailable: scope.healthStatus.IsMailServerAvailable
-                    },
-                    {
-                        name: 'Message Queue Server',
-                        isAvailable: scope.healthStatus.IsMessageQueueServerAvailable
-                    },
-                    {
-                        name: 'Elasticsearch Server',
-                        isAvailable: scope.healthStatus.IsElasticsearchServerAvailable
+                console.log(scope.healthStatus);
+                scope.$watch('healthStatus', function (healthStatus) {
+                    if (angular.isUndefined(healthStatus)) {
+                        return;
                     }
-                ];
+                    scope.servers = [
+                        {
+                            name: 'Mail Server',
+                            isAvailable: healthStatus.IsMailServerAvailable
+                        },
+                        {
+                            name: 'Message Queue Server',
+                            isAvailable: healthStatus.IsMessageQueueServerAvailable
+                        },
+                        {
+                            name: 'Elasticsearch Server',
+                            isAvailable: healthStatus.IsElasticsearchServerAvailable
+                        }
+                    ];
+
+                });
             }
         }
     }
